e8f04a28639deb20f606c2e126327034b48201a7
Varsha Kandi <kandivarsha09@gmail.com>
2024-07-06 20:38:24 -0400

    Fixed page not found issues with top-rated, popular and favorite pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -52,13 +52,13 @@ export const Footer = () => {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">Cinemate</Link>. All Rights Reserved.
           </span>
-          <div className="flex mt-4 sm:justify-center sm:mt-0">
-            <a href="#" target="_blank" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+          <div className="flex mt-4 sm:justify-center sm:items-center sm:mt-0 justify-center items-center">
+            <Link to="#" target="_blank" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                 <path fillRule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clipRule="evenodd" />
               </svg>
               <span className="sr-only">Facebook Page</span>
-            </a>
+            </Link>
             <Link to="#" className="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
               <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M12.5 8.8v1.7a3.7 3.7 0 0 1 3.3-1.7c3.5 0 4.2 2.2 4.2 5v5.7h-3.2v-5c0-1.3-.2-2.8-2.1-2.8-1.9 0-2.2 1.3-2.2 2.6v5.2H9.3V8.8h3.2ZM7.2 6.1a1.6 1.6 0 0 1-2 1.6 1.6 1.6 0 0 1-1-2.2A1.6 1.6 0 0 1 6.6 5c.3.3.5.7.5 1.1Z" clipRule="evenodd" />
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,11 +7,11 @@ export const MovieCard = ({movie}) => {
 
   return (
     <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 m-3">
-        <Link to={`movie/${id}`}>
+        <Link to={`/movie/${id}`}>
             <img className="rounded-t-lg" src={image} alt="poster" />
         </Link>
         <div className="p-5">
-            <Link to={`movie/${title}`}>
+            <Link to={`/movie/${id}`}>
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
             </Link>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{overview}</p>

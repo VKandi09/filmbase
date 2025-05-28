427ba7462fde700a23751a5094c893921ec72f98
VKandi09 <kandivarsha09@gmail.com>
2025-05-28 12:09:07 -0400

    Added dynamic date on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,51 +6,10 @@ export const Footer = () => {
     <footer className="bg-white dark:bg-gray-900">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
-          {/* <div className="mb-6 md:mb-0">
-            <Link to="https://flowbite.com/" className="flex items-center">
-              <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 me-3" alt="FlowBite Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </Link>
-          </div>
-          <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <Link to="https://flowbite.com/" className="hover:underline">Flowbite</Link>
-                </li>
-                <li>
-                  <Link to="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <Link to="https://github.com/themesberg/flowbite" className="hover:underline ">Github</Link>
-                </li>
-                <li>
-                  <Link to="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</Link>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-              <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                <li className="mb-4">
-                  <Link to="#" className="hover:underline">Privacy Policy</Link>
-                </li>
-                <li>
-                  <Link to="#" className="hover:underline">Terms &amp; Conditions</Link>
-                </li>
-              </ul>
-            </div>
-          </div> */}
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <Link to="/" className="hover:underline">FilmBase</Link>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">&copy; {new Date().getFullYear()} <Link to="/" className="hover:underline">FilmBase</Link>. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:items-center sm:mt-0 justify-center items-center">
             <Link to="#" target="_blank" className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
